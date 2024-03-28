b67786cfa3bf56720cab551a0c93ab3bf1ec21ed
Natouille <ngouveia@edenschool.fr>
2024-03-28 14:47:16 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,30 @@
 let count = 0;
-let multiplier = 10;
+let curseurPrice = 10;
+let multiplier = 1;
 
 const cookie = document.querySelector("#cookie");
 const countDisplay = document.querySelector("#count");
+const curseurBtn = document.querySelector("#curseur-btn");
+const curseurPriceDisplay = document.querySelector("#curseur-price");
 
 function cookieClick() {
     count = count + (1 * multiplier);
-    countDisplay.textContent = count;
+    countDisplay.textContent = Math.trunc(count);
     cookie.classList.add("clicked");
     setTimeout(() => {
         cookie.classList.remove("clicked");
     }, 200);
 }
 
+function curseurClick() {
+    if(count >= curseurPrice) {
+        count = count - curseurPrice;
+        curseurPrice = curseurPrice + 1;
+        countDisplay.textContent = Math.trunc(count);
+        curseurPriceDisplay.textContent = curseurPrice;
+        setInterval(cookieClick, 60000);
+    }
+}
+
 cookie.addEventListener("click", cookieClick);
+curseurBtn.addEventListener("click", curseurClick);
