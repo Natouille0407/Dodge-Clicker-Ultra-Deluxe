e651db95c3d518cefccfd6dde58bab4662374f10
Natouille <ngouveia@edenschool.fr>
2024-03-29 12:34:26 +0100

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,7 @@ let count = 0;
 let multiplier = 1;
 let clickNb = 0;
 let curseurPrice = 10;
+let curseurMultiplier = 1;
 
 const cookie = document.querySelector("#cookie");
 const countDisplay = document.querySelector("#count");
@@ -13,11 +14,22 @@ if (localStorage.getItem('dodge') !== null) {
     countDisplay.textContent = Math.trunc(count);
 }
 
+if (localStorage.getItem('curseurPrice') !== null) {
+    curseurPrice = parseInt(localStorage.getItem('curseurPrice'));
+    curseurPriceDisplay.textContent = Math.trunc(curseurPrice);
+}
+
+if (localStorage.getItem('clickNb') !== null) {
+    clickNb = parseInt(localStorage.getItem('clickNb'))
+    for(let i = 0; i < clickNb; i++) {
+        setInterval(cookieClick, 2500)
+    }
+}
+
 function updateStorage() {
     localStorage.setItem('dodge', count);
     localStorage.setItem('clickNb', clickNb);
     localStorage.setItem('curseurPrice', curseurPrice);
-    localStorage.setItem('curseur', clickNb);
 }
 
 function updateCountDisplay() {
@@ -38,11 +50,13 @@ function cookieClick() {
 function curseurClick() {
     if (count >= curseurPrice) {
         count -= curseurPrice;
-        curseurPrice++;
+        curseurPrice += 10 + curseurMultiplier;
+        curseurMultiplier++
         curseurPriceDisplay.textContent = curseurPrice;
         clickNb += 1;
         updateCountDisplay();
         updateStorage();
+        setInterval(cookieClick, 2500)
     }
 }
 
